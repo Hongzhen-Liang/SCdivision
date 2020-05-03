67a183507d396f3f9f5f7a1d16d8430bdfb9a59a
sinscry <sinscryw@gmail.com>
2020-05-03 21:15:31 +0800

    完成出库

--- 后端代码/flask_wx.py
+++ 后端代码/flask_wx.py
@@ -11,12 +11,12 @@ import time
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return "It''s working"
 @app.route('/send',methods=['POST'])
 def send():
     upload = str(json.loads(request.values.get("upload")))
     upload_list = upload.split(',')
-    print(upload_list)
+    # print(upload_list)
     conn = pymysql.connect(host='127.0.0.1', user='scnu',password='scd',database='scd',charset='utf8')
     # 得到一个可以执行SQL语句的光标对象
     cursor = conn.cursor()
@@ -46,5 +46,41 @@ def send():
         res='数据提交失败'
         return json.dumps(res)
     return json.dumps(upload)
+
+@app.route('/delete',methods=['POST'])
+def delete():
+    upload = str(json.loads(request.values.get("upload")))
+    upload_list = upload.split(',')
+    print(upload_list)
+    conn = pymysql.connect(host='127.0.0.1', user='scnu',password='scd',database='scd',charset='utf8')
+    # 得到一个可以执行SQL语句的光标对象
+    cursor = conn.cursor()
+    # 定义要执行的SQL语句
+    sql = "update depot set status=0 where id=%s;"
+    # 执行SQL语句
+    id1,type1,price,submission_date = upload_list[0],upload_list[1],upload_list[2],time.strftime("%Y%m%d")  # 'AA3002059','C',203.5,time.strftime("%Y%m%d")
+    try:
+        res=cursor.execute(sql,[id1])
+        # 涉及写操作要注意提交
+        conn.commit()
+    except pymysql.err.IntegrityError:
+        res=2
+    finally:
+        # 关闭光标对象
+        cursor.close()
+        # 关闭数据库连接
+        conn.close()
+    if res==1:
+        # print res
+        res='出库成功'
+        return json.dumps(res)
+    else:
+        if res==2:
+            return json.dumps('数据重复')
+        # print(res)
+        res='数据提交失败'
+        return json.dumps(res)
+    return json.dumps(upload)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True,port=80)
