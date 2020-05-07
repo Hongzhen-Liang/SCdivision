75b1b7a8f6b454aad6d8b4054962f1c0d8da1473
xiang <993329135@qq.com>
2020-05-07 20:15:27 +0800

    实现了微信一键登陆
    测试的时候要在自己的手机打开开发版的小程序点击右上角三个点的图标取消授权登陆，不然就一直不会有授权登陆的界面，且其它界面会登陆是否到期，自动跳转到index界面登陆

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,13 +4,19 @@ const app = getApp()
 
 Page({
   data: {
+    isHide: false,
     motto: 'This is Position Authority explanation',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    realName:'default',
-    imageUri:'../../images/touxiang/default.png',
-    explanation: { 'depotManager': '仅允许出库入库', 'front':'仅改价格和判定货物能否出货','admin':'无约束','driver':'仅更改货物位置信息'}
+    realName: 'default',
+    imageUri: '../../images/touxiang/default.png',
+    explanation: {
+      'depotManager': '仅允许出库入库',
+      'front': '仅改价格和判定货物能否出货',
+      'admin': '无约束',
+      'driver': '仅更改货物位置信息'
+    }
   },
   //事件处理函数
   bindViewTap: function() {
@@ -18,74 +24,114 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+  onLoad: function() {
     var that = this;
-    wx.request({
-      url: 'http:/127.0.0.1/user',//这部分需要完善服务器操作
-      method:'GET',
-      data:{
-        wxid:'',//匹配码
-        position:'position',
-        realName:'',
-      },
-      success(res)
-      {
-        var pos=res.data.position;
-        var name =res.data.realName;
-        that.setData(
-        {
-          imageUri:'../../images/touxiang/'+this.pos+'.png',
-          motto:'权限说明：'+that.data.explanation[this.pos],
-          realName:this.name
+    wx.getSetting({
+      success: function(res) {
+        wx.showToast({
+          title: '登陆中',
+          icon: 'loading'
         })
-      },
-      //测试
-      fail(res)
-      {
-        var pos = 'admin';
-        var name ='华南师团';
-        that.setData(
-        {
-          imageUri: '../../images/touxiang/' + pos + '.png',
-          motto: '权限说明：' + that.data.explanation[pos]+'，此为服务器反馈失败测试',
-          realName: name
-        })
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: function(res) {
+              // 用户已经授权过,不需要显示授权页面,所以不需要改变 isHide 的值
+              // 根据自己的需求有其他操作再补充
+              // 我这里实现的是在用户授权成功后，调用微信的 wx.login 接口，从而获取code
+              wx.login({
+                success: res => {
+                  // 获取到用户的 code 之后：res.code
+                  var code =  res.code;
+
+                  wx.request({
+                    url: 'http:/127.0.0.1/wx_flask.py', //这部分需要完善服务器操作
+                    method: 'GET',
+                    data: {
+                      wxid: code, //匹配码
+                      position: 'position',
+                      realName: '',
+                    },
+                    success(res) {
+                      var pos = res.data.position;
+                      var name = res.data.realName;
+                      that.setData({
+                        imageUri: '../../images/touxiang/' + this.pos + '.png',
+                        motto: '权限说明：' + that.data.explanation[this.pos],
+                        realName: this.name
+                      })
+                    },
+                    //测试
+                    fail(res) {
+                      var pos = 'admin';
+                      var name = '华南师团';
+                      that.setData({
+                        imageUri: '../../images/touxiang/' + pos + '.png',
+                        motto: '权限说明：' + that.data.explanation[pos] + '，此为服务器反馈失败测试',
+                        realName: name
+                      })
+                    }
+                  })
+                  // 可以传给后台，再经过解析获取用户的 openid
+                  // 或者可以直接使用微信的提供的接口直接获取 openid ，方法如下：
+                  // wx.request({
+                  //     // 自行补上自己的 APPID 和 SECRET
+                  //     url: 'https://api.weixin.qq.com/sns/jscode2session?appid=自己的APPID&secret=自己的SECRET&js_code=' + res.code + '&grant_type=authorization_code',
+                  //     success: res => {
+                  //         // 获取到用户的 openid
+                  //         console.log("用户的openid:" + res.data.openid);
+                  //     }
+                  // });
+                }
+              });
+            }
+          });
+        } else {
+          // 用户没有授权
+          // 改变 isHide 的值，显示授权页面
+          wx.hideTabBar({
+            
+          });
+          that.setData({
+            isHide: true
+          });
+        }
+        wx.hideToast();
       }
-    })
+    });
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  bindGetUserInfo: function(e) {
+    if (e.detail.userInfo) {
+      //用户按了允许授权按钮
+      var that = this;
+      // 获取到用户的信息了，打印到控制台上看下
+      console.log("用户的信息如下：");
+      console.log(e.detail.userInfo);
+      //授权成功后,通过改变 isHide 的值，让实现页面显示出来，把授权页面隐藏起来
+      that.setData({
+        isHide: false
+      });
+      wx.showTabBar({
+        
+      })
+      //跳转至send
+      wx.reLaunch({
+        url: 'index',
+      })
+    } else {
+      //用户按了拒绝按钮
+      wx.showModal({
+        title: '警告',
+        content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
+        showCancel: false,
+        confirmText: '返回授权',
+        success: function(res) {
+          // 用户没有授权成功，不需要改变 isHide 的值
+          if (res.confirm) {
+            console.log('用户点击了“返回授权”');
+          }
+        }
+      });
+    }
   }
 
 })
--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -6,6 +6,15 @@ Page({
     logs: []
   },
   onLoad: function () {
+    wx.checkSession({
+      success: function(res) {},
+      fail: function(res) {
+        wx.reLaunch({
+          url: '../index/index'
+        })
+      },
+      complete: function(res) {},
+    })
     this.setData({
       logs: (wx.getStorageSync('logs') || []).map(log => {
         return util.formatTime(new Date(log))
